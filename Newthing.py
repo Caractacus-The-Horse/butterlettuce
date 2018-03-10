e0e8ef518d3141d72ca978fa7e26c8705517160d
Caractacus-The-Horse <cole.dylewski@gmail.com>
2018-03-10 10:24:31 -0500

    blah blah blah

--- Newthing.py
+++ Newthing.py
@@ -1,3 +1,24 @@
-print("fart")
-x= 5
-print(5)
+
+
+import random
+
+file = open("C:\\Users\\cole.dylewski\\Documents\\Python stuff\\areacodes.txt","r")
+junk = file.readlines()
+
+
+junk.sort()
+
+
+for num in range(0,2):
+    temp = junk[random.randint(0,len(junk))]
+    areacode = "("+temp[0:3]+") "
+    x = 0
+    numbah=""
+    while (x <7):
+
+        y = random.randint(0,9)
+        numbah+=str(y)
+        if(x==2):numbah+="-"
+        x+=1
+        numbr = areacode + numbah
+    print(numbr)
